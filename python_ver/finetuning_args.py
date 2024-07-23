10032696ada00b3354d16819ebe09ccceb5c9714
YuanhuanO <yuanhuandeng@brandeis.edu>
2024-07-23 15:50:05 -0700

    Remove .DS_Store files

---
+++ python_ver/finetuning_args.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass, field
+from typing import Optional
+
+
+@dataclass
+class LoraArgs:
+    lora_alpha: Optional[int] = field(default=None)
+    lora_dropout: Optional[float] = field(default=None)
+    lora_rank: Optional[int] = field(default=8)
+    lora_target: Optional[str] = field(default=None)
+
+@dataclass
+class QLoraArgs:
+    qlora_alpha: Optional[int] = field(default=None)
+    qlora_dropout: Optional[float] = field(default=None)
+
+@dataclass
+class FinetuningArgs(LoraArgs, QLoraArgs):
+    training_precision: str = field(default='mixed')
+    is_fsdp: bool = field(default=True)
