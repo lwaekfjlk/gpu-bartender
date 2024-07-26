d55df0773cf4edbdc978811c04f2184fc21bb8c5
YuanhuanO <yuanhuandeng@brandeis.edu>
2024-07-25 19:29:10 -0700

    gitignore update

--- gpu_bartender/server/__init__.py
+++ gpu_bartender/server/__init__.py
@@ -1,8 +1,8 @@
-from .calculator import VRAMCalculator
-from .data_args import DataArgs
-from .finetuning_args import FinetuningArgs
-from .model_args import ModelArgs
-from .optimizer_args import OptimizerArgs
+from calculator import VRAMCalculator
+from data_args import DataArgs
+from finetuning_args import FinetuningArgs
+from model_args import ModelArgs
+from optimizer_args import OptimizerArgs
 
 __all__ = [
     'VRAMCalculator',
