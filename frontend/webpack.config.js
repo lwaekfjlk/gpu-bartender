4dce5ac06438c05acb7bf3ecbdb4e0fab9e46c65
Olivia Deng <113553401+YuanhuanO@users.noreply.github.com>
2024-07-31 19:59:48 -0700

    fix(frontend): npm localhost3000 not found (#28)
    * Fixed app.py
    
    * fix
    
    * odify gitignore
    
    * Update README.md
    
    * fix access
    
    * Resolved conflicts
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix pre-commit error
    
    * update the code and revoert the merge
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: lwaekfjlk <1125027232@qq.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,21 +1,30 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/frontend/scripts.ts',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
+  entry: './src/main.ts',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000,
   },
 };
